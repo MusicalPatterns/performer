2adb6da1b404784222592f15b0956715bee6a3e2
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-29 21:35:04 -0800

    1.0.42: offer a nicer interface for apps which just want to get a specific (immersive) party started immediately

--- src/interface/setupPerformer.ts
+++ src/interface/setupPerformer.ts
@@ -1,13 +1,27 @@
 import { loadAllSamples } from '../performance'
 import { activateContextInMobileBrowserEnvironments, setupThreadsListener, setupTimeControls } from '../setup'
+import { enableImmersiveAudio } from './enableImmersiveAudio'
+import { restart } from './restart'
 import { SetupPerformerParameters } from './types'
 
+// tslint:disable-next-line:no-empty
+const noop: VoidFunction = (): void => {
+}
+
 const setupPerformer: (setupPerformerParameters: SetupPerformerParameters) => void =
-    ({ onUpdate }: SetupPerformerParameters): void => {
+    ({ onUpdate = noop, autoStart }: SetupPerformerParameters): void => {
         loadAllSamples()
         activateContextInMobileBrowserEnvironments()
         setupThreadsListener()
         setupTimeControls(onUpdate)
+
+        if (autoStart) {
+            if (autoStart.vrb) {
+                enableImmersiveAudio({ vrb: autoStart.vrb })
+            }
+
+            restart(autoStart.threadSpecs)
+        }
     }
 
 export {
--- src/interface/types.ts
+++ src/interface/types.ts
@@ -1,12 +1,19 @@
 import { Vrb } from 'vrb'
 import { OnUpdate } from '../setup'
+import { ThreadSpec } from '../types'
 
 interface EnableImmersiveAudioParameters {
     vrb?: Vrb,
 }
 
+interface AutoStart {
+    threadSpecs: ThreadSpec[],
+    vrb?: Vrb,
+}
+
 interface SetupPerformerParameters {
-    onUpdate: OnUpdate,
+    autoStart?: AutoStart,
+    onUpdate?: OnUpdate,
 }
 
 export {
