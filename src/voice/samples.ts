58e08135c4dc2fb4b3c3e0d2132613126ba5804f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-10 09:29:31 -0800

    v1.0.67: i think the problem was that we did not actually await for the buffer to be saved to the samples object before returning it

--- src/voice/samples.ts
+++ src/voice/samples.ts
@@ -29,20 +29,22 @@ const getTimbreUrl: (timbre: SampleName) => ModulePath =
     }
 
 const load: (timbre: SampleName) => void =
-    async (timbre: SampleName): Promise<void> => {
-        const request: XMLHttpRequest = new XMLHttpRequest()
-        const url: ModulePath = getTimbreUrl(timbre)
-        request.open('GET', url, true)
-        request.responseType = 'arraybuffer'
+    async (timbre: SampleName): Promise<void> =>
+        new Promise((resolve: VoidFunction): void => {
+            const request: XMLHttpRequest = new XMLHttpRequest()
+            const url: ModulePath = getTimbreUrl(timbre)
+            request.open('GET', url, true)
+            request.responseType = 'arraybuffer'
 
-        request.onload = async (): Promise<void> => {
-            const audioData: ArrayBuffer = request.response as ArrayBuffer
-            await context.decodeAudioData(audioData, (buffer: AudioBuffer): void => {
-                samples[ timbre ] = buffer
-            })
-        }
-        request.send()
-    }
+            request.onload = async (): Promise<void> => {
+                const audioData: ArrayBuffer = request.response as ArrayBuffer
+                await context.decodeAudioData(audioData, (buffer: AudioBuffer): void => {
+                    samples[ timbre ] = buffer
+                    resolve()
+                })
+            }
+            request.send()
+        })
 
 export {
     getOrLoad,
