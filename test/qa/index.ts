d31bbfc68c7825ecc5927d884cd78a810056d1f9
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-19 16:09:05 -0800

    v1.0.79: use performer QA pattern instead of template for QA'ing

--- test/qa/index.ts
+++ test/qa/index.ts
@@ -1,10 +1,10 @@
-import snapshot from '@musical-patterns/pattern-template'
+import { performerQaSnapshot } from '@musical-patterns/pattern-performer-qa'
 // tslint:disable-next-line:only-import-index-for-test-from-src
 import { setupPerformer } from '../../src'
 
 setupPerformer({
     autoStart: {
-        threadSpecs: snapshot,
+        threadSpecs: performerQaSnapshot,
     },
 })
     .then()
