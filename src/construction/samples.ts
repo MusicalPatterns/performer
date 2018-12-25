96d5c8d4618476dac5dc2c26fe44f6040a8cdeec
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-25 12:02:20 -0800

    fix webpack warning

--- src/construction/samples.ts
+++ src/construction/samples.ts
@@ -4,9 +4,7 @@ import { ModulePath, SampleName } from './types'
 // tslint:disable:no-any
 const samples: { [x in SampleName]: AudioBuffer } = {} as any
 
-declare const require: (modulePath: ModulePath) => string
-
-const getOrLoad: (sampleName: SampleName) => Promise<AudioBuffer>  =
+const getOrLoad: (sampleName: SampleName) => Promise<AudioBuffer> =
     async (sampleName: SampleName): Promise<AudioBuffer> => {
         if (!samples[ sampleName ]) {
             await load(sampleName)
@@ -15,24 +13,11 @@ const getOrLoad: (sampleName: SampleName) => Promise<AudioBuffer>  =
         return samples[ sampleName ]
     }
 
-const getSampleUrl: (sampleName: SampleName) => ModulePath =
-    (sampleName: SampleName): ModulePath => {
-        let inTest: boolean = false
-        Object.keys(require)
-            .forEach((key: string): void => {
-                if (key === 'extensions') {
-                    inTest = true
-                }
-            })
-
-        return inTest ? '' : require(`../../samples/${sampleName}.wav`)
-    }
-
 const load: (sampleName: SampleName) => Promise<void> =
     async (sampleName: SampleName): Promise<void> =>
         new Promise((resolve: VoidFunction): void => {
             const request: XMLHttpRequest = new XMLHttpRequest()
-            const url: ModulePath = getSampleUrl(sampleName)
+            const url: ModulePath = require(`../../samples/${sampleName}.wav`)
             request.open('GET', url, true)
             request.responseType = 'arraybuffer'
 
