496624e2167ece55c306d54cdec39c615209a4f0
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-07 14:08:15 -0800

    1.0.111: kill sound upon stop

--- src/interface/stop.ts
+++ src/interface/stop.ts
@@ -1,9 +1,17 @@
 import { to } from '@musical-patterns/utilities'
 import { BatchAction, batchActions } from 'redux-batched-actions'
-import { ActionType, store } from '../state'
+import { ActionType, ImmutableState, StateKeys, store } from '../state'
+import { Thread } from '../types'
 
 const stop: VoidFunction =
     (): void => {
+        const state: ImmutableState = store.getState() as ImmutableState
+        const threads: Thread[] = state.get(StateKeys.THREADS)
+
+        threads.forEach((thread: Thread) => {
+            thread.voice.stopNote()
+        })
+
         const batchedAction: BatchAction = batchActions([
             { type: ActionType.SET_TIME, data: to.Time(0) },
             { type: ActionType.SET_PAUSED, data: true },
