e90bb5853c345d58a297a826b440c28c322f95ca
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-26 17:22:07 -0800

    1.0.150: standardize webpack.browser more

---
+++ src/start.ts
@@ -0,0 +1,35 @@
+import { snapshot } from '@musical-patterns/pattern-performer-qa'
+import { to } from '@musical-patterns/utilities'
+import { enableImmersiveAudio, setTime, setupPerformer, togglePaused } from './interface'
+
+const setupQa: () => Promise<void> =
+    async (): Promise<void> => {
+        // tslint:disable-next-line:no-unsafe-any
+        await setupPerformer({ threadSpecs: snapshot })
+        const toggleImmersiveAudioHandler: VoidFunction = enableImmersiveAudio()
+
+        const setTimeButton: HTMLElement = document.createElement('button')
+        setTimeButton.innerText = 'Set Time (to 14800)'
+        // tslint:disable-next-line:no-magic-numbers
+        setTimeButton.addEventListener('click', async () => setTime(to.Time(14800)))
+        document.body.appendChild(setTimeButton)
+
+        const stopButton: HTMLElement = document.createElement('button')
+        stopButton.innerText = 'Stop'
+        stopButton.addEventListener('click', stop)
+        document.body.appendChild(stopButton)
+
+        const togglePausedButton: HTMLElement = document.createElement('button')
+        togglePausedButton.innerText = 'Toggle Paused'
+        togglePausedButton.addEventListener('click', togglePaused)
+        document.body.appendChild(togglePausedButton)
+
+        const toggleImmersiveAudioButton: HTMLElement = document.createElement('button')
+        toggleImmersiveAudioButton.innerText = 'Toggle Immersive Audio'
+        toggleImmersiveAudioButton.addEventListener('click', toggleImmersiveAudioHandler)
+        document.body.appendChild(toggleImmersiveAudioButton)
+    }
+
+setupQa()
+    .then()
+    .catch()
--- webpack.browser.js
+++ webpack.browser.js
@@ -2,14 +2,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { PERFORMER_PORT } = require('@musical-patterns/utilities')
 
 module.exports = {
-    entry: './test/qa/performer.js',
+    entry: './src/start.ts',
     devServer: {
         open: true,
         port: PERFORMER_PORT,
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Musical Patterns - Performer QA',
+            title: 'Musical Patterns',
+            meta: { viewport: 'width=device-width' },
         }),
     ],
 }
