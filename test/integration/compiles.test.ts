ba29b3376b5f6df6d44872c30fc711992b7f4574
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-01 11:54:19 -0800

    1.0.170: base integration tests on test/integration

--- test/integration/compiles.test.ts
+++ test/integration/compiles.test.ts
@@ -1,6 +1,6 @@
-import { page } from '../../setup'
+import { page } from '../setup'
 
-describe('clock', () => {
+describe('it compiles', () => {
     it('since worker files can be a bit finnicky, and i cannot test in node, confirm that the code compiles', async (done: DoneFn) => {
         expect(await page.waitForSelector('button'))
             .toBeTruthy()
