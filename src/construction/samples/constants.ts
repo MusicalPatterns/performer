6b554225f3b090755acf0e98f24ac04ba3106149
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-05 12:45:47 -0800

    1.0.173: scientific pitch

--- src/construction/samples/constants.ts
+++ src/construction/samples/constants.ts
@@ -1,6 +1,13 @@
 // tslint:disable no-magic-numbers
 
-import { Frequency, Semitones, to } from '@musical-patterns/utilities'
+import {
+    Frequency,
+    SCIENTIFIC_PITCHES,
+    ScientificPitchNoteName,
+    ScientificPitchOctaveNumber,
+    Semitones,
+    to,
+} from '@musical-patterns/utilities'
 
 const UP_TWO_SEMITONES: Semitones = to.Semitones(2)
 const DOWN_ONE_SEMITONE: Semitones = to.Semitones(-1)
@@ -8,7 +15,8 @@ const UP_ONE_OCTAVE_IN_SEMITONES: Semitones = to.Semitones(12)
 const UP_TWO_OCTAVES_IN_SEMITONES: Semitones = to.Semitones(24)
 const DOWN_TWO_OCTAVES_IN_SEMITONES: Semitones = to.Semitones(-24)
 
-const STANDARDIZED_SAMPLE_PITCH_OF_C5: Frequency = to.Frequency(523.25)
+const STANDARDIZED_SAMPLE_PITCH_OF_C5: Frequency =
+    SCIENTIFIC_PITCHES[ ScientificPitchNoteName.C ][ ScientificPitchOctaveNumber._5 ]
 
 export {
     UP_ONE_OCTAVE_IN_SEMITONES,
