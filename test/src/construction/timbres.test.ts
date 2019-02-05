6dfe2c64f15d8f464ac2fa19f38d4aa117f1ec6a
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-05 14:11:23 -0800

    1.0.174: use new typed entries

--- test/src/construction/timbres.test.ts
+++ test/src/construction/timbres.test.ts
@@ -1,9 +1,10 @@
+import { keys } from '@musical-patterns/utilities'
 import { OscillatorName, SampleName } from '../../../src/indexForTest'
 
 describe('timbres', () => {
     it('all timbres are accessible, because there is no overlap between oscillators and samples, such that the sample version of that timbre name would override the oscillator version (or vice versa)', () => {
-        Object.keys(OscillatorName)
-            .forEach((oscillatorName: string): void => {
+        keys(OscillatorName)
+            .forEach((oscillatorName: keyof typeof OscillatorName): void => {
                 expect(oscillatorName in SampleName)
                     .toBeFalsy(`${oscillatorName} is the name of both an oscillator timbre and a sample timbre. Please rename one or the other.`)
             })
