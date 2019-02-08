510b55d26dba630b87e4e49ed778a68e9c4f5b20
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-08 10:58:05 -0800

    1.0.180: typed iterators

--- src/construction/applyHomePosition.ts
+++ src/construction/applyHomePosition.ts
@@ -1,12 +1,16 @@
-import { apply, Coordinate, CoordinateElement, from, to } from '@musical-patterns/utilities'
+import { apply, Coordinate, CoordinateElement, from, map, Ordinal, to } from '@musical-patterns/utilities'
 import { Note } from '../types'
 
 const applyHomePosition: (note: Note, homePosition: Coordinate) => Note =
     (note: Note, homePosition: Coordinate): Note =>
         ({
             ...note,
-            position: note.position.map((coordinateElement: CoordinateElement, index: number) =>
-                apply.Translation(coordinateElement, to.Translation(from.CoordinateElement(homePosition[ index ])))),
+            position: map(note.position, (coordinateElement: CoordinateElement, index: Ordinal) =>
+                apply.Translation(
+                    coordinateElement,
+                    to.Translation(from.CoordinateElement(apply.Ordinal(homePosition, index))),
+                ),
+            ),
         })
 
 export {
