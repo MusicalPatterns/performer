42539eba8cbf08a8474937b573a04be7a38bd924
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-10 16:40:07 -0800

    1.0.182: nominal types overhaul

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,9 +1,9 @@
 // tslint:disable no-magic-numbers
 
-import { MILLISECONDS_PER_SECOND } from '@musical-patterns/utilities'
+import { from, MILLISECONDS_PER_SECOND } from '@musical-patterns/utilities'
 
 const TARGET_FPS: number = 120
-const TIME_STEP: number = MILLISECONDS_PER_SECOND / TARGET_FPS
+const TIME_STEP: number = from.Integer(MILLISECONDS_PER_SECOND) / TARGET_FPS
 
 export {
     TIME_STEP,
