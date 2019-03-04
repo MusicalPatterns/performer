b331062c143729f6a4b0fa02b4fc096aa7dd9af5
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-04 11:31:21 -0800

    1.0.191: tslint cleanup

--- src/performance/context.ts
+++ src/performance/context.ts
@@ -12,8 +12,10 @@ if (AudioContext) {
     context = new AudioContext()
 }
 else {
-    // tslint:disable-next-line max-line-length
-    logMessageToScreen('Sorry, but the Web Audio API is not supported by your browser. Please, consider upgrading to the latest version or downloading Google Chrome or Mozilla Firefox')
+    logMessageToScreen(`
+Sorry, but the Web Audio API is not supported by your browser.
+Please, consider upgrading to the latest version or downloading Google Chrome or Mozilla Firefox
+    `)
 }
 
 export {
