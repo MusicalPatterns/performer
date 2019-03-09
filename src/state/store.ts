fdc3e50adeb97511f3ed18f8f0f479123c863c31
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-09 02:09:51 -0800

    1.0.198: use DRYed reducer builder

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,16 +1,15 @@
-// tslint:disable no-any
-
+import { buildReducer } from '@musical-patterns/utilities'
 import { applyMiddleware, compose, createStore, Store } from 'redux'
 import { BatchAction, batchDispatchMiddleware, enableBatching } from 'redux-batched-actions'
 import { initialState } from './initial'
-import { reducer } from './reducer'
 import { Action, ImmutableState } from './types'
 
 // @ts-ignore
+// tslint:disable:next-line no-any
 const composeEnhancers: any = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store: Store<ImmutableState, Action | BatchAction> = createStore(
-    enableBatching(reducer),
+    enableBatching(buildReducer({ initialState })),
     initialState,
     composeEnhancers(applyMiddleware(batchDispatchMiddleware)),
 )
