1d2e06204ba3e87fd21d54e08c66d09735736da8
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-10 12:04:54 -0700

    1.0.201: improve failure case for integration test

--- test/integration/compiles.test.ts
+++ test/integration/compiles.test.ts
@@ -1,8 +1,9 @@
+import { elementExists } from '@musical-patterns/utilities'
 import { page } from '../setup'
 
 describe('it compiles', () => {
     it('since worker files can be a bit finnicky, and i cannot test in node, confirm that the code compiles', async (done: DoneFn) => {
-        expect(await page.waitForSelector('button'))
+        expect(await elementExists(page, 'button'))
             .toBeTruthy()
 
         done()
